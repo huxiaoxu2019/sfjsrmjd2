a5b581ae78eb6feea35c42df0b8eb509964a461c
huxiaoxu2019 <admin@ihuxu.com>
2020-11-03 00:44:09 +0800

    first commit

---
+++ c5/5.1.3.cpp
@@ -0,0 +1,18 @@
+// 字符串流
+#include<iostream>
+#include<algorithm>
+#include<string>
+#include<sstream>
+
+using namespace std;
+
+int main() {
+    string line;
+    while (getline(cin, line)) {
+        int sum = 0, x;
+        stringstream ss(line);
+        while (ss >> x) sum += x;
+        cout << sum << endl;
+    }
+    return 0;
+}
---
+++ c5/5.2.2.cpp
@@ -0,0 +1,8 @@
+// vector
+#include<cstdio>
+#include<algorithm>
+using namespace std;
+const int maxn = 1e4;
+int main() {
+    
+}
