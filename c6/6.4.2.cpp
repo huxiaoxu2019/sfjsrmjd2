88ea7f4c74c8a0207ac0873a0f7ab8f81ab32eaa
huxiaoxu2019 <admin@ihuxu.com>
2020-11-05 13:30:10 +0800

    :wrench: add codes

--- c6/6.4.2.cpp
+++ c6/6.4.2.cpp
@@ -1,38 +1,50 @@
 #include<bits/stdc++.h>
+#include<sstream>
 using namespace std;
 struct Node {
     int r, c, d;
     Node(int r = 0, int c = 0, int d = 0) : r(r), c(c), d(d) {}
 };
+ostream& operator << (ostream& out, const Node& p) {
+    cout << "(" << p.r << "," << p.c << "," << p.d << ")";
+    return out;
+}
 int dist[10][10][4];
 Node p[10][10][4];
 // 0 1 2 3 -> r(E) d(S) l(W) u(N)
 int d[5] = {0, 1, 0, -1, 0};
-unordered_map<char, int> ds{'E':0, 'S':1, 'W':2, 'N':3};
-unordered_map<char, int> ts{'F':0, 'L':1, 'R':2};
+unordered_map<char, int> ds{{'E',0}, {'S',1}, {'W',2}, {'N',3}};
+unordered_map<char, int> ts{{'F',0}, {'L',1}, {'R',2}};
 bool can[10][10][4][3];
 queue<Node> q;
 Node walk(Node u, int ti) {
     if (ti == 1) u.d = (u.d + 3) % 4;
     if (ti == 2) u.d = (u.d + 1) % 4;
     Node v(u.r + d[u.d], u.c + d[u.d + 1], u.d);
-}
-void solve(Node& u, Node& v) {
-    q.push(u);
+    return v;
+};
+void solve(Node& uu, Node& vv) {
+    //cout << "solve:" << endl;
+    q.push(uu);
+    //cout << "uu:" << uu << " vv:" << vv << endl;
     while (!q.empty()) {
-        auto n = q.front();
+        auto u = q.front();
         q.pop();
+        //cout << "pop item:" << u << endl;
         for (int ti = 0; ti < 3; ++ti) {
             if (can[u.r][u.c][u.d][ti] == false) continue;
-            auto n = walk(u, ti);
-            if (dist[n.r][n.c][n.d] <= dist[u.r][u.c][u.d] + 1) continue;
-            dist[n.r][n.c][n.d] = dist[u.r][u.c][u.d] + 1;
-            p[n.r][n.c][n.d] = u;
-            if (n.r == v.r && n.c == v.c) {
-                v.d = n.d;
+            //cout << "can" << endl;
+            auto v = walk(u, ti);
+            //cout << "ti:" << ti << " v:" << v << endl;
+            if (dist[v.r][v.c][v.d] <= dist[u.r][u.c][u.d] + 1) continue;
+            //cout << "dist yes" << endl;
+            dist[v.r][v.c][v.d] = dist[u.r][u.c][u.d] + 1;
+            p[v.r][v.c][v.d] = u;
+            if (v.r == vv.r && v.c == vv.c) {
+                vv.d = v.d;
                 return;
             }
-            q.push(n);
+            q.push(v);
         }
     }
 }
@@ -47,7 +59,7 @@ void print_ans(Node& v, string& name) {
     while(true) {
         ans.push_back(n);
         auto nn = p[n.r][n.c][n.d];
-        if (d[n.r][n.c][n.d] == 0) {
+        if (dist[n.r][n.c][n.d] == 0) {
             ans.push_back(nn);
             break;
         }
@@ -64,22 +76,26 @@ int main() {
         memset(dist, 0x3f, sizeof dist);
         string name;
         cin >> name;
+        if (name == "END") break;
         Node u, v;
         char cd;
         cin >> u.r >> u.c  >> cd >> v.r >> v.c;
         v.d = -1;
-        int u.d = ds[cd];
+        u.d = ds[cd];
+        //cout << "r:" << u.r << " c:" << u.c << " d:" << cd << " r:" << v.r << " c:" << v.c << endl;
         string line;
+        getline(cin, line); // \n
         while (true) {
-            cin >> line;
-            streamstring sline(line);
+            getline(cin, line);
+            //cout << "line:" << line << endl;
+            stringstream sline(line);
             int r, c;
             sline >> r;
             if (r == 0) {
                 int lr = u.r, lc = u.c;
                 u.r += d[u.d], u.c += d[u.d + 1];
                 p[u.r][u.c][u.d] = {lr, lc, u.d};
-                d[u.r][u.c][u.d] = 0;
+                dist[u.r][u.c][u.d] = 0;
                 solve(u, v);
                 // print 
                 print_ans(v, name);
@@ -87,12 +103,15 @@ int main() {
             }
             sline >> c;
             string limit;
+            //cout << "r:" << r << " c:" << c << endl;
             while (sline >> limit) {
                 if (limit == "*") {
                     break;
                 }
                 int dir = ds[limit[0]];
+                //cout << " d:" << limit[0] << " dir:" << dir << endl;
                 for (int i = 1; i < limit.size(); ++i) {
+                    //cout << "ts:" << ts[limit[i]] << endl;
                     can[r][c][dir][ts[limit[i]]] = true;
                 }
             }
